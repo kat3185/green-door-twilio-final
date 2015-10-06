64af69f350824d960a83f159a421c9b354e0a89e
kat3185 <Ken.Thomas.3185@gmail.com>
2015-10-06 17:58:17 -0400

    sinatra base in app rb

--- app.rb
+++ app.rb
@@ -7,67 +7,69 @@ class Game < ActiveRecord::Base
   #model
 end
 
-def find_game(text)
-  game_list = Array.new
-  Game.all.each do |game|
-    if game.short_title.downcase.include?(text.downcase)
-      game_list << game
+class App < Sinatra::Base
+  def find_game(text)
+    game_list = Array.new
+    Game.all.each do |game|
+      if game.short_title.downcase.include?(text.downcase)
+        game_list << game
+      end
+    end
+    game_list
+  end
+
+  def create_response(games, text)
+    if games.empty?
+      "I'm sorry, I don't know that game!"
+    elsif games.length == 1
+      "Visit #{games.first.url} to play #{games.first.title}.  Enjoy!"
+    elsif games.length == 2
+      "If you are looking for #{games.first.title}, visit #{games.first.url}, if you meant #{games.second.title}, visit #{games.second.url}"
+    else
+      "I know several games that have #{text} in them.  Could you be more specific?"
     end
   end
-  game_list
-end
 
-def create_response(games, text)
-  if games.empty?
-    "I'm sorry, I don't know that game!"
-  elsif games.length == 1
-    "Visit #{games.first.url} to play #{games.first.title}.  Enjoy!"
-  elsif games.length == 2
-    "If you are looking for #{games.first.title}, visit #{games.first.url}, if you meant #{games.second.title}, visit #{games.second.url}"
-  else
-    "I know several games that have #{text} in them.  Could you be more specific?"
-  end
-end
+  get "/sms-green-door" do
 
-get "/sms-green-door" do
+    body = params[:Body]
 
-  body = params[:Body]
+    games = find_game(body)
 
-  games = find_game(body)
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message create_response(games, body)
+    end
 
-  twiml = Twilio::TwiML::Response.new do |r|
-    r.Message create_response(games, body)
+    twiml.text
   end
 
-  twiml.text
-end
-
-get "/" do
-  redirect "/games"
-end
-
-get "/games" do
-  game_list = Game.all
-  erb :index, locals: {game_list: game_list}
-end
-
-get "/games/:id" do
-  game = Game.find(params[:id])
-  erb :edit, locals: {game: game}
-end
-
-post "/games" do
-  my_game = Game.new(params[:game])
-
-  if my_game.save
+  get "/" do
     redirect "/games"
-  else
-    erb :index, locals: { game: game }
   end
-end
 
-post "/DestroyGame" do
-  game_to_delete = Game.find(params[:game_id])
-  game_to_delete.destroy
-  redirect "/games"
+  get "/games" do
+    game_list = Game.all
+    erb :index, locals: {game_list: game_list}
+  end
+
+  get "/games/:id" do
+    game = Game.find(params[:id])
+    erb :edit, locals: {game: game}
+  end
+
+  post "/games" do
+    my_game = Game.new(params[:game])
+
+    if my_game.save
+      redirect "/games"
+    else
+      erb :index, locals: { game: game }
+    end
+  end
+
+  post "/DestroyGame" do
+    game_to_delete = Game.find(params[:game_id])
+    game_to_delete.destroy
+    redirect "/games"
+  end
 end
