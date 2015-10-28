73e9fa6d2c65754436cb64dcc2ed0e33a5628c41
kat3185 <Ken.Thomas.3185@gmail.com>
2015-10-28 00:10:47 -0400

    extracted models from app.rb

--- app.rb
+++ app.rb
@@ -3,39 +3,9 @@ require "twilio-ruby"
 require "sinatra/activerecord"
 require "./config/environments"
 
-class Game < ActiveRecord::Base
-end
-
-class Text
-  attr_reader :games
-  def initialize(body)
-    @body = body.downcase
-  end
-
-  def find_games
-    @games = Game.where("LOWER(short_title) LIKE ?", "%#{@body}%").to_a
-  end
-
-  def determine_response
-    if @games.length == 1
-      "Visit #{@games.first.url} to play #{@games.first.title}.  Enjoy!"
-    elsif @games.length == 2
-      "If you are looking for #{@games.first.title}, visit #{@games.first.url}, if you meant #{@games.second.title}, visit #{@games.second.url}"
-    elsif @games.empty?
-        "I'm sorry, I don't know that game!"
-    else
-      "I know several games that have #{@body} in them.  Could you be more specific?"
-    end
-  end
-
-  def return_response
-    find_games
-    determine_response
-  end
-end
+Dir['app/**/*.rb'].each { |file| require_relative file }
 
 get "/sms-green-door" do
-
   recieved_text = Text.new(params[:Body])
 
   twiml = Twilio::TwiML::Response.new do |r|
